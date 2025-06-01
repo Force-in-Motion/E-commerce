0780c4c916c4e5524e42ef00a8229557eb794351
Force-inMotion <canzdao@gmail.com>
2025-06-01 11:58:36 +0300

    feat(project): add the necessary libraries are installed

---
+++ main.py
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+
+
+
+# engine.connect() - контекстный менеджер, при помощи которого открывается соединение с базой данных
+# with engine.connect() as connect:
+#     result = connect.execute(text())
