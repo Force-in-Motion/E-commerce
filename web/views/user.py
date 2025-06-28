1560983cfc112c7262ccb0a258fd6ed90b9d5b89
Force-inMotion <canzdao@gmail.com>
2025-06-28 18:54:05 +0300

    feat(project): add user views and router

--- web/views/user.py
+++ web/views/user.py
@@ -1,5 +1,6 @@
 from fastapi import HTTPException
 from fastapi import APIRouter
+from sqlalchemy.util import await_only
 
 from web.shemas.user import User
 from service.database.crud.user import UserAdapter
@@ -10,10 +11,38 @@ from service.database.crud.user import UserAdapter
 router = APIRouter(prefix='/users', tags=['users'])
 
 
+@router.get('/')
+async def get_users(session):
+    try:
+        return await UserAdapter.get_users(session)
+
+    except HTTPException as e:
+        return {'status': e}
+
+
+@router.get('/{id}')
+async def get_user_by_id(session, id: int):
+    try:
+        return await UserAdapter.get_user_by_id(session, id)
+
+    except HTTPException as e:
+        return {'status': e}
+
+
 @router.post('/')
 async def add_user(session, user: User) -> dict:
     try:
         await UserAdapter.add_user(session, user)
+        return {'status': 'ok', 'status_code': 200}
+
+    except HTTPException as e:
+        return {'status': e}
+
+
+@router.delete('/{id}')
+async def del_user_by_id(session, id: int):
+    try:
+        await UserAdapter.del_user(session, id)
         return {'status': 'ok', 'status_code': 200}
 
     except HTTPException as e:
