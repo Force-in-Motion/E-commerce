0f531479721b25f7be495ab60a5323005427673b
Force-inMotion <canzdao@gmail.com>
2025-07-06 20:59:20 +0300

    feat(project): add new dependency func user_by_id

--- tools/dependencies.py
+++ tools/dependencies.py
@@ -4,14 +4,34 @@ from fastapi import Path, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from service.database import db_connector
-from service.database.models import Product as Product_model
-from service.database.crud import ProductAdapter as pa
+from service.database.models import Product as Product_model, User as User_model
+from service.database.crud import ProductAdapter as pa, UserAdapter as ua
 
 
-async def product_by_id(id: Annotated[int, Path], session: AsyncSession = Depends(db_connector.session_dependency)) -> Product_model:
+async def product_by_id(
+    id: Annotated[int, Path],
+    session: AsyncSession = Depends(db_connector.session_dependency),
+) -> Product_model:
     product_model = await pa.get_product_by_id(id, session)
 
     if product_model is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Product with this id not found')
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Product with this id not found",
+        )
 
     return product_model
+
+
+async def user_by_id(
+    id: Annotated[int, Path],
+    session: AsyncSession = Depends(db_connector.session_dependency),
+) -> User_model:
+    user_model = await ua.get_user_by_id(session, id)
+
+    if user_model is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User with this id not found"
+        )
+
+    return user_model
--- web/schemas/user.py
+++ web/schemas/user.py
@@ -3,11 +3,15 @@ from typing import Annotated
 from annotated_types import MaxLen, MinLen, Ge, Le, Gt
 
 
-class User(BaseModel):
-    model_config = ConfigDict(from_attributes=True)
+class UserInput(BaseModel):
 
-    id: Annotated[int, Ge(1)]
     name: Annotated[str, MinLen(3), MaxLen(30)]
     floor: Annotated[str, MinLen(3), MaxLen(12)]
     age: Annotated[int, Ge(12), Le(120)]
     email: EmailStr
+
+
+class UserOutput(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: Annotated[int, Ge(1)]
