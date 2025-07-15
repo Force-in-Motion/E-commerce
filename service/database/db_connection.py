ab613d00c57b8336885f6e327772038d33765ab9
Force-inMotion <canzdao@gmail.com>
2025-07-15 16:26:00 +0300

    refactor(models): add notes for DBConnector class

--- service/database/db_connection.py
+++ service/database/db_connection.py
@@ -1,23 +1,28 @@
 from sqlalchemy.ext.asyncio import (
     create_async_engine,
     async_sessionmaker,
-    async_scoped_session)
+    async_scoped_session,
+)
 from asyncio import current_task
 
 from settings import db_settings
 
 
-
-
 class DBConnector:
-    def __init__(self, url, echo):
-        self.__engine = create_async_engine(url=url, echo=echo)
-        self.__session_factory = async_sessionmaker(
-            bind=self.__engine,
-            autoflush=False,
-            autocommit=False,
-            expire_on_commit=False)
 
+    def __init__(self, url, echo):
+        """При инициализации объекта, создает движок- create_async_engine и фабрику сессий - async_sessionmaker"""
+
+        self.__engine = create_async_engine(  # Асинхронный движок, обязательным параметром ожидает url - путь к базе данных, в данном случае получаемый из db_settings
+            url=url, echo=echo
+        )
+
+        self.__session_factory = async_sessionmaker(  # Фабрика сессий, ожидает:
+            bind=self.__engine,  # движок
+            autoflush=False,  # Отключает автоматическую отправку изменений в базу данных перед выполнением запросов
+            autocommit=False,  # транзакции не завершаются автоматически.
+            expire_on_commit=False,  # Отключает сброс (expiration) объектов в сессии после фиксации транзакции (commit).
+        )
 
     def get_engine(self):
         """
@@ -26,15 +31,12 @@ class DBConnector:
         """
         return self.__engine
 
-
     def __get_scoped_session(self):
         session = async_scoped_session(
-            session_factory=self.__session_factory,
-            scopefunc=current_task
+            session_factory=self.__session_factory, scopefunc=current_task
         )
 
         return session
-
 
     async def session_dependency(self):
         session = self.__get_scoped_session()
@@ -42,5 +44,11 @@ class DBConnector:
         await session.remove()
 
 
+db_connector = DBConnector(url=db_settings.db_url, echo=db_settings.echo)
 
-db_connector = DBConnector(db_settings.db_url, db_settings.echo)
+
+# Когда expire_on_commit=True (по умолчанию), SQLAlchemy помечает все объекты в сессии как "устаревшие" после commit,
+# и их данные будут запрошены из базы при следующем обращении. Это может вызвать дополнительные запросы.
+
+# С expire_on_commit=False объекты остаются в кэше сессии (Identity Map),
+# и можно продолжать работать с ними без лишних запросов.
