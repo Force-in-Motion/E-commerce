25323f2027674b02252443d539c32a00a2128a21
Force-inMotion <canzdao@gmail.com>
2025-07-15 15:07:59 +0300

    refactor(models): add notes for base model

--- service/database/models/base.py
+++ service/database/models/base.py
@@ -1,14 +1,18 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
 
 
-
 class Base(DeclarativeBase):
-    __abstract__ = True
+    """Создает родительский класс для всех таблицы базы данных"""
+
+    __abstract__ = True  # Указывает алхимии, что такой таблицы в БД быть не должно, она абстрактная
 
     @classmethod
+    # Этот декоратор позволяет реализовать логику, которая автоматически генерирует название таблицы в БД,
+    # которое соответствует названию класса SQLAlchemy, в котором она описана
     @declared_attr.directive
     def __tablename__(cls) -> str:
         return cls.__name__.title()
 
+    # id присутствует во всех таблицах, поэтому его можно указать в родительской,
+    # чтобы при наследовании он по умолчанию был уже во всех дочерних таблицах
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-
