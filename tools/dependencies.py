e3f06067af41b108fe90484c99a57590bd7490dd
Force-inMotion <canzdao@gmail.com>
2025-08-10 13:38:49 +0300

    feat(view/post): "add endpoints get_post_by_id, get_posts_by_user_id

--- tools/dependencies.py
+++ tools/dependencies.py
@@ -4,12 +4,15 @@ from fastapi import Path, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from service.database import db_connector
+from service.database.crud.post import PostAdapter
 from service.database.models import (
     Product as Product_model,
     User as User_model,
     Profile as Profile_model,
+    Post as Post_model,
 )
 from service.database.crud import ProductAdapter, UserAdapter, ProfileAdapter
+from web.schemas import PostOutput
 
 
 async def product_by_id(
@@ -72,3 +75,44 @@ async def profile_by_user_id(
         )
 
     return profile_model
+
+
+async def post_by_id(
+    id: Annotated[int, Path],
+    session: AsyncSession = Depends(db_connector.session_dependency),
+) -> Post_model:
+    """
+    Возвращает конкретный пост по его id
+    :param id:
+    :param session:
+    :return:
+    """
+    post_model = await PostAdapter.get_post_by_id(session, id)
+
+    if post_model is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Post with this id not found"
+        )
+
+    return post_model
+
+
+async def posts_by_user_id(
+    user_id: Annotated[int, Path],
+    session: AsyncSession = Depends(db_connector.session_dependency),
+) -> list[Post_model]:
+    """
+    Возвращает все посты пользователя по его id
+    :param user_id: id конкретного пользователя
+    :param session: объект сессии, который получается путем выполнения зависимости (метода session_dependency объекта db_connector)
+    :return: list[PostOutput]
+    """
+    list_user_posts = await PostAdapter.get_posts_by_user_id(session, user_id)
+
+    if list_user_posts is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Posts with this user_id not found",
+        )
+
+    return list(list_user_posts)
--- web/views/post.py
+++ web/views/post.py
@@ -1,22 +1,45 @@
-
-
 from fastapi import APIRouter, status, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from service.database import db_connector
 from service.database.crud.post import PostAdapter
+from tools.dependencies import posts_by_user_id, post_by_id
 from web.schemas import PostOutput
 
 router = APIRouter()
 
 
-@router.get('/', response_model=list[PostOutput], status_code=status.HTTP_200_OK)
+@router.get("/", response_model=list[PostOutput], status_code=status.HTTP_200_OK)
 async def get_posts(
-        session: AsyncSession = Depends(db_connector.session_dependency),
+    session: AsyncSession = Depends(db_connector.session_dependency),
 ) -> list[PostOutput]:
     """
-    Обрабатывает запрос с фронт энда на получение списка всех профилей пользователей
+    Обрабатывает запрос с фронт энда на получение списка всех постов пользователей
     :param session: объект сессии, который получается путем выполнения зависимости (метода session_dependency объекта db_connector)
-    :return: Список всех профилей пользователей
+    :return: Список всех постов пользователей
     """
     return await PostAdapter.get_all_posts(session)
+
+
+@router.get("/{id}", response_model=PostOutput, status_code=status.HTTP_200_OK)
+async def get_post_by_id(
+    post: PostOutput = Depends(post_by_id),
+) -> PostOutput:
+    """
+     Обрабатывает запрос с фронт энда на получение конкретного поста по его id
+    :param post: объект PostOutput, который получается путем выполнения зависимости (метода post_by_id)
+    :return: конкретный пост по его id
+    """
+    return post
+
+
+@router.get("/{user_id}", response_model=PostOutput, status_code=status.HTTP_200_OK)
+async def get_posts_by_user_id(
+    posts: list[PostOutput] = Depends(posts_by_user_id),
+) -> list[PostOutput]:
+    """
+    Обрабатывает запрос с фронт энда на получение списка всех постов конкретного пользователя
+    :param posts: список объектов PostOutput, который получается путем выполнения зависимости (метода posts_by_user_id)
+    :return: список всех постов пользователя
+    """
+    return posts
