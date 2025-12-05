28392d4941dbf9f0fed082d4892548b074e5f7ad
Force-in-Motion <canzdao@gmail.com>
2025-12-05 13:11:30 +0300

    feat(types): add Enum types user role

--- app/tools/types.py
+++ app/tools/types.py
@@ -1,10 +1,21 @@
 from typing import TypeVar, TYPE_CHECKING
+import enum
 
 if TYPE_CHECKING:
     from pydantic import BaseModel
     from app.interface import ARepo
     from app.models import Base
 
+
+# Абстрактные типы модели, схемы, адаптера
 DBModel = TypeVar("DBModel", bound="Base")
 Adapter = TypeVar("Adapter", bound="ARepo")
 PDScheme = TypeVar("PDScheme", bound="BaseModel")
+
+
+# Определяет возможные роли пользователей
+class UserRole(str, enum.Enum):
+    god = 'god'
+    user = "user"
+    admin = "admin"
+    moderator = "moderator"
