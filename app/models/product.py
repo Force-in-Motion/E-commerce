0376eac795f3a2f63d8253a695e85539588cc3e9
Force-in-Motion <canzdao@gmail.com>
2025-12-09 11:34:54 +0300

    refactor(models): refactor product model, add mixin timestamp

--- app/models/product.py
+++ app/models/product.py
@@ -1,80 +1,42 @@
 from datetime import datetime
 from typing import TYPE_CHECKING
 
-from sqlalchemy import String, Integer, DateTime, func
+from sqlalchemy import String, Integer, DateTime, Text, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from . import Base
 
 if TYPE_CHECKING:
-    from . import OrderProducts
-    from . import CartProduct
+    from app.models import Base, OrderProducts, CartProduct, TimestampMixin
 
 
-class Product(Base):
+class Product(Base, TimestampMixin):
     """Класс, описывающий мета информацию таблицы Product"""
 
-    __tablename__ = "products"  # Название таблицы в БД
+    __tablename__ = "products"
 
-    # Описание мета информации таблицы
     name: Mapped[str] = mapped_column(
-        String,
+        String(255),
         nullable=False,
     )
 
     description: Mapped[str] = mapped_column(
-        String,
+        Text,
         nullable=False,
     )
 
-    price: Mapped[int] = mapped_column(
-        Integer,
+    price: Mapped[str] = mapped_column(
+        String(255),
         nullable=False,
     )
 
-    created_at: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True),
-        server_default=func.now(),
-    )
-
-    # Заказы, содержащие этот продукт
     orders: Mapped[list["OrderProducts"]] = relationship(
         back_populates="product",
-        lazy="select",
         cascade="all, delete-orphan",
-    )  # Полноценная модель-связка
+        lazy="select",
+    )
 
     carts: Mapped[list["CartProduct"]] = relationship(
         back_populates="product",
-        lazy="select",
         cascade="all, delete-orphan",
+        lazy="select",
     )
-
-
-# Mapped — это обобщённый тип (generic type) из модуля sqlalchemy.orm,
-# для аннотации типов атрибутов модели. Он указывает, что атрибут класса (например, name)
-# связан с колонкой в базе данных и имеет определённый Python-тип (например, str).
-
-# mapped_column — это функция из sqlalchemy.orm, для определения колонок и их параметров (тип, ограничения, индексы)
-# в декларативных моделях. Она создаёт объект колонки и связывает его с атрибутом, аннотированным Mapped.
-
-# Mapped и mapped_column используются для определения модели, которая регистрируется в Base.metadata.
-
-# relationship создаёт атрибут в классе модели, который позволяет:
-# Получать связанные объекты (например, список постов пользователя через user.posts).
-# Автоматически загружать связанные данные из базы, когда ты обращаешься к этому атрибуту.
-
-# Это говорит SQLAlchemy, что у объекта Product есть атрибут orders, который возвращает список объектов Order, связанные с этим продуктом.
-# back_populates="products" указывает обратную связь: в модели Order есть атрибут products, который ссылается на список объектов Product.
-
-
-# Сквозная связь через secondary - Удобно, когда таблица-связка (OrderProducts) ничего больше не хранит, кроме order_id и product_id.
-# Тогда ты просто работаешь через order.products или product.orders, и тебе не важно, как устроена связка.
-# Минус: ты не можешь добавить дополнительные данные (например, count или скидку на товар в заказе). Таблица не содержит ничего кроме внешних ключей
-
-# Полноценная модель-связка - Этот вариант нужен, если у связки есть дополнительные поля — например:
-# count (количество товара в заказе),
-# price на момент заказа,
-# discount,
-# статус (например, "подарок").
-# В этом случае OrderProducts становится полноценной ассоциативной сущностью, а не просто связкой.
