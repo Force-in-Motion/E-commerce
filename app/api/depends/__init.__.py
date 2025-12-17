cd24496b4a31dbf24b0a18df6cc2b92ce75f003c
Force-in-Motion <canzdao@gmail.com>
2025-12-17 14:41:16 +0300

    rework(depends): depends user profile and post transferred to depends

--- app/api/depends/__init.__.py
+++ app/api/depends/__init.__.py
@@ -1,8 +1,15 @@
 __all__ = [
     "Inspector",
     "UserAuth",
+    "UserAuth",
+    "UserCrud",
+    "ProfileCrud",
+    "PostCrud",
 ]
 
 
 from app.api.depends.inspect import Inspector
-from app.api.depends.user.user import UserAuth
+from app.api.depends.user import UserAuth
+from app.api.depends.user import UserAuth, UserCrud
+from app.api.depends.profile import ProfileCrud
+from app.api.depends.post import PostCrud
