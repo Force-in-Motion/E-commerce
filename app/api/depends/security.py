cafaab95aaaa1eed0877601c38e5a62593fd7b3f
Force-in-Motion <canzdao@gmail.com>
2025-12-23 15:20:11 +0300

    rework(routers): depends oau2scheme moved to a separate module to eliminate duplication in each router

---
+++ app/api/depends/security.py
@@ -0,0 +1,19 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends
+
+from app.core import db_connector
+from app.tools import HTTPErrors, UserRole
+from app.api.depends.user import UserAuth
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/user/auth/login")
+
+async def admin_guard(
+    token: str = Depends(oauth2_scheme),
+    session: AsyncSession = Depends(db_connector.session_dependency),
+):
+    user = await UserAuth.get_current_user_by_access(token, session)
+
+    if user.role != UserRole.admin:
+        raise HTTPErrors.not_admin
